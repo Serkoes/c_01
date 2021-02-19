519138ed1b66e34ee8570df971d99d3be98f5162
Chaurus Chekov <cchekov@ox-h1.msk.21-school.ru>
2021-02-19 19:22:31 +0300

    add headers

--- ex05/ft_putstr.c
+++ ex05/ft_putstr.c
@@ -1,7 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cchekov cchekov@student.21-school.ru       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/19 19:12:23 by cchekov           #+#    #+#             */
+/*   Updated: 2021/02/19 19:13:00 by cchekov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
 void ft_putstr(char *str)
 {
-	for (long unsigned int i=0;i<=sizeof(str)/2;i++)
-	{	
-		write(1,&str[i],1);
+	char buff;
+	int i;
+
+	i = 0;
+	while (1)
+	{
+		buff = *(str + i);
+		if (buff == '\0')
+		{
+			break;
+		} 
+		write(1, &buff, 1);
+		i = i + 1;		
 	}
 }
